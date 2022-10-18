bfc63f6ddde4ad22abafccbb3e08e2d49f75069a
parthkumardpatel <115606615+parthkumardpatel@users.noreply.github.com>
2022-10-19 00:02:47 +0530

    Add files via upload

---
+++ breakncontinue.py
@@ -0,0 +1,18 @@
+for i in range(1,10):
+    if i==5:
+        break
+    print(i)
+
+print()
+
+for i in range(1,10):
+    if i==3 or i==8:
+        continue
+    print(i)
+
+print()
+
+for i in range(1,10):
+    if i==3 or i==6 or i==8:
+        continue
+    print(i)
---
+++ sumofloop.py
@@ -0,0 +1,26 @@
+'''
+n=int(input("Enter N : "))
+
+sum=0
+for i in range(1,n+1):
+    sum=sum+i
+
+print("Sum : ",sum)
+'''
+
+'''
+n = int(input("Enter No. "))
+
+evensum=0
+oddsum=0
+
+for i in range(1,n+1):
+    if i%2==0:
+        evensum=evensum+i
+    else:
+        oddsum=oddsum+i
+
+print("Even Sum : ",evensum)
+print("Odd Sum : ",oddsum)
+
+'''
